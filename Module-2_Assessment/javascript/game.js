a21d028475e20049214feda0f1f5591034ad9b68
AyDy Burling <aydy.you@gmail.com>
2020-05-24 03:31:58 -0500

    cleaning up code

---
+++ Module-2_Assessment/javascript/game.js
@@ -0,0 +1,26 @@
+//create array of five names
+//randomly select name from list
+//seperate letters from word
+//display letters to be guessed with _ _ _
+
+//EVENT LISTENERS==============================================================
+//only allow valid guesses - lowercase letters only
+//display only valid guesses on _ _ _
+
+//GAME RULES====================================================================
+//play starts with 6 lives
+//decrease lives with every wrong letter guess
+//display wrong guesses
+//if same wrong letter is guessed again in one game, display "you have already guessed that letter"
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++
+//player loses
+//when no lives left display "Loser" in modal
+//display button to play again
+//play loser music or clip
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++
+//player winner
+// modal of "Winner!"
+// play winning clip of quote or music
+//display button to play again
