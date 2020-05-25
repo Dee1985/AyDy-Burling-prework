011f83c750ac8fee1fadc53897a2c52abd5a53cc
AyDy Burling <aydy.you@gmail.com>
2020-05-24 19:36:53 -0500

    updates

--- JavaScript_Basics_Assessment/activity-3.js
+++ JavaScript_Basics_Assessment/activity-3.js
@@ -1,37 +1,12 @@
 // Activity 3: JavaScript Arrays and Loops:
 
-// let studentNames = ["Mario", "Yoshi", "Koopa"];
-
-// for (let i = 0; i < 3; i++) {
-//   const name = prompt("Please enter another name");
-//   studentNames.push(name);
-// }
-
-// for (let i = 0; i < studentNames.length; i++) {
-//   console.log(studentNames[i]);
-// }
-
 let studentNames = ["Mario", "Yoshi", "Koopa"];
 
-function createStudentList() {
-  for (let i = 0; i < 3; i++) {
-    const name = prompt("Please enter another name");
-    studentNames.push(name);
-  }
-  for (let i = 0; i < studentNames.length; i++) {
-    console.log(createStudentList[i]);
-  }
+for (let i = 0; i < 3; i++) {
+  const name = prompt("Please enter another name");
+  studentNames.push(name);
 }
-createStudentList();
 
-// function createStudentList() {
-//   for (let i = 0; i < 3; i++) {
-//     const name = prompt("Please enter another name");
-//     studentNames.push(name);
-//   }
-//   for (let i = 0; i < studentNames.length; i++) {
-//     console.log(studentNames);
-//     return studentNames;
-//   }
-// }
-// createStudentList();
+for (let i = 0; i < studentNames.length; i++) {
+  console.log(studentNames[i]);
+}
