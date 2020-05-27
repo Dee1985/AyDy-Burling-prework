cfc120a22632fe06d0ebdc790f0cff5782c63ef4
AyDy Burling <aydy.you@gmail.com>
2020-05-26 20:44:33 -0500

    keypress

--- Module-2_Assessment/javascript/game.js
+++ Module-2_Assessment/javascript/game.js
@@ -5,37 +5,6 @@ const wrongLetters = document.getElementById("wrongLetters");
 const modal = document.getElementById("modal");
 const hint = document.getElementById("hint");
 
-// 1) create array of names & quotes
-let quotes = [
-  {
-    name: "hagrid",
-    quote: '"You\'re a wizard Harry"',
-  },
-  {
-    name: "dumbledore",
-    quote:
-      '"Let us step into the night and pursue that flighty temptress, adventure."',
-  },
-  {
-    name: "harry",
-    quote: '"Time will not slow down when something unpleasent lies ahead."',
-  },
-  {
-    name: "sirius",
-    quote:
-      '"If you want to know what a man’s like, take a good look at how he treats his inferiors, not his equals."',
-  },
-  {
-    name: "hermione",
-    quote:
-      '"Books! And cleverness! There are more important things!- Friendship! And Bravery!"',
-  },
-  {
-    name: "ron",
-    quote:
-      '"Of all the trees we could’ve hit, we had to get one that hits back."',
-  },
-];
 const randomQuotePicked = quotes[Math.floor(Math.random() * quotes.length)];
 const { name, quote } = randomQuotePicked;
 
@@ -52,14 +21,14 @@ function pickQuote() {
   //   console.log(hiddenLetters);
 
   //display letters to be guessed with _ _ _
-  //   for (let i = 0; i < hiddenLetters.length; i++) {
-  //     hiddenLetters.splice(i, 1, "_");
-  //     console.log("hiddenLetters: ", name, hiddenLetters[i]);
-  //   }
   for (let i = 0; i < hiddenLetters.length; i++) {
-    hiddenLetters[i] = "_";
+    hiddenLetters.splice(i, 1, "_");
     console.log("hiddenLetters: ", name, hiddenLetters[i]);
   }
+  //   for (let i = 0; i < hiddenLetters.length; i++) {
+  //     hiddenLetters[i] = "_";
+  //     console.log("hiddenLetters: ", name, hiddenLetters[i]);
+  //   }
 
   document.getElementById("quote").innerHTML = quote;
   document.getElementById("hiddenName").innerHTML = hiddenLetters.join(" ");
@@ -83,7 +52,7 @@ $(document).ready(function () {
 
 // Show hidden word
 document.addEventListener("keydown", (e) => {
-  console.log("keypressed!");
+  console.log("keypressed=", e.key);
 });
 
 //play starts with 6 lives
---
+++ Module-2_Assessment/javascript/quotes.js
@@ -0,0 +1,32 @@
+// 1) create array of names & quotes
+
+const quotes = [
+  {
+    name: "hagrid",
+    quote: '"You\'re a wizard Harry"',
+  },
+  {
+    name: "dumbledore",
+    quote:
+      '"Let us step into the night and pursue that flighty temptress, adventure."',
+  },
+  {
+    name: "harry",
+    quote: '"Time will not slow down when something unpleasent lies ahead."',
+  },
+  {
+    name: "sirius",
+    quote:
+      '"If you want to know what a man’s like, take a good look at how he treats his inferiors, not his equals."',
+  },
+  {
+    name: "hermione",
+    quote:
+      '"Books! And cleverness! There are more important things!- Friendship! And Bravery!"',
+  },
+  {
+    name: "ron",
+    quote:
+      '"Of all the trees we could’ve hit, we had to get one that hits back."',
+  },
+];
