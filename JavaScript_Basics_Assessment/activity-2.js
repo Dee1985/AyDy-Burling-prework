a5b59d6c96b9dc8bfda9dad24f49409807131b04
AyDy Burling <aydy.you@gmail.com>
2020-05-30 02:40:20 -0500

    fixed incoming changes alert in js assessment activity2

--- JavaScript_Basics_Assessment/activity-2.js
+++ JavaScript_Basics_Assessment/activity-2.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-// Activity 2: JavaScript Variables, Alerts, Prompts, and Conditionals:
-
 function promptUser() {
   const userName = prompt("Please enter your first name");
   const charLimit = 4;
@@ -12,19 +9,6 @@ function promptUser() {
   } else {
     alert("Your name is exactly 4 characters long");
   }
-=======
-function promptUser() {
-	const userName = prompt('Please enter your first name');
-	const charLimit = 4;
-
-	if (userName.length > charLimit) {
-		alert('Your name is greater than 4 characters long');
-	} else if (userName.length < charLimit) {
-		alert('Your name is less than 4 characters long');
-	} else {
-		alert('Your name is exactly 4 characters long'); 
-	}
->>>>>>> 47481f30e797aae20f55713378ed63a505fd2d55
 }
 
 promptUser();
