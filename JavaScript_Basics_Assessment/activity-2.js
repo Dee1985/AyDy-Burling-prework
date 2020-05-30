adc1865fc763cbef4d0f43e0675e5ffc51c3b97c
AyDy Burling <aydy.you@gmail.com>
2020-05-30 02:44:49 -0500

    decided to not make activity a function

--- JavaScript_Basics_Assessment/activity-2.js
+++ JavaScript_Basics_Assessment/activity-2.js
@@ -1,14 +1,10 @@
-function promptUser() {
-  const userName = prompt("Please enter your first name");
-  const charLimit = 4;
+const userName = prompt("Please enter your first name");
+const charLimit = 4;
 
-  if (userName.length > charLimit) {
-    alert("Your name is greater than 4 characters long");
-  } else if (userName.length < charLimit) {
-    alert("Your name is less than 4 characters long");
-  } else {
-    alert("Your name is exactly 4 characters long");
-  }
+if (userName.length > charLimit) {
+  alert("Your name is greater than 4 characters long");
+} else if (userName.length < charLimit) {
+  alert("Your name is less than 4 characters long");
+} else {
+  alert("Your name is exactly 4 characters long");
 }
-
-promptUser();
